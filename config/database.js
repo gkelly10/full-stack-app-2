43b2011da8bf918176c8a274afd27cc3a65207e5
gkelly10 <gabriellekelly10101@gmail.com>
2024-11-17 21:45:21 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://gabbie:Colby@cluster0.mggu4.mongodb.net/fullstackapp2?retryWrites=true&w=majority&appName=Cluster0', 
+    'dbName': 'fullstackapp2'
+};
